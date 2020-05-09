59cef727d303121ab784666410bf52f6e38a5958
snorlexK <now97kim@naver.com>
2020-05-09 13:30:25 +0900

    project created

---
+++ src/test/java/itc/hoseo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package itc.hoseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
